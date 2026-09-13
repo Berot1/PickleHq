55a7946e6f201404da41eee5729872b3d662baf8
Gil Bernard F. Maglinte <127652117+Berot1@users.noreply.github.com>
2026-09-13 15:34:32 +0800

    Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Reservation::class);
     }
+
+    public function passkeys()
+    {
+        return $this->hasMany(Passkey::class); // Adjust based on your actual relationship
+    }
 }
